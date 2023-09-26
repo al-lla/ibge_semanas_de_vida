1dcaba4587acd46d2b00247bbffd8b832c534c00
Alexandre Alves <alexandre.alves97@gmail.com>
2023-09-26 12:17:17 -0300

    criação de venv e arquivo do app

---
+++ app.py
@@ -0,0 +1,9 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from pywaffle import Waffle
+import streamlit as st
+
+st.set_page_config(page_title='Quantas semanas você tem?')
+
+st.write('# Hello, World!')
